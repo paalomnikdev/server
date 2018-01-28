a6e04c810fe88ff52ee5ff7985c1fb89a10d7f79
Ivan Cherniy <paalomnik@gmail.com>
2018-01-28 20:12:34 +0200

    prevent role edit and delete

--- main.py
+++ main.py
@@ -143,7 +143,9 @@ class UserAdmin(sqla.ModelView):
 
 class RoleAdmin(sqla.ModelView):
     column_searchable_list = ('name', 'description')
-
+    can_delete = False
+    can_edit = False
+    
     def is_accessible(self):
         return current_user.has_role('admin')
 
